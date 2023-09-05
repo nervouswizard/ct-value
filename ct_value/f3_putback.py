8695432d414c4b1af311707746481435f0c80726
LaB <2753xiaolala@gmail.com>
2023-09-06 07:57:36 +0800

    using same method as test

--- ct_value/f3_putback.py
+++ ct_value/f3_putback.py
@@ -8,10 +8,10 @@ def putback(file_path_data, read_path_pvalue, save_path, label_column, print=pri
     # pvalue data
     Pdata = pd.read_csv(file_path_data, low_memory=False)
     # CTvalue data
-    CTdata = pd.read_csv(file_path_data, low_memory=False)
-
+    #CTdata = pd.read_csv(file_path_data, low_memory=False)
+    CTdata = Pdata.copy()
     pvaluePath = os.path.join(read_path_pvalue, 'statistic', 'map_table')
-
+    '''
     for c, col in enumerate(Pdata.columns.to_list()):
         if col in label_column: continue
         print("Process", c, col)
@@ -32,7 +32,16 @@ def putback(file_path_data, read_path_pvalue, save_path, label_column, print=pri
             feature = Pdata.at[i, col]
             Pdata.at[i, col] = pvalueDict.get(feature, 0)
             CTdata.at[i, col] = Pdata.at[i, col]-0.5
-
+    '''
+    for c, col in enumerate(Pdata.columns.to_list()):
+        if col in label_column: continue
+        print(f'replace {c} {col}')
+        pvalueDataFilenameByColunm = os.path.join(pvaluePath,str(c)+'_'+col+'.csv')
+        pvalueDF = pd.read_csv(pvalueDataFilenameByColunm, header=0, low_memory=False)
+        print('benign P-value Data Shape', pvalueDF.shape)
+        Pdata[col] = Pdata[col].map(pvalueDF.set_index('value')['pvalue']).fillna(0).astype(float)
+        CTdata[col] = Pdata[col] - 0.5
+        del pvalueDF
     #存檔
     Pdata.to_csv(os.path.join(save_path, 'p-value', 'train.csv'), index=None)
     CTdata.to_csv(os.path.join(save_path, 'ct-value', 'train.csv'), index=None)
